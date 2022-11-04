99e2014779afceb1e381c225f403dc5450ced315
Andre Sabes <andresabes@outlook.com>
2022-11-03 22:19:49 -0300

    first commit

---
+++ src/scenes/global/Topbar.jsx
@@ -0,0 +1,56 @@
+import { Box, IconButton, InputBase, useTheme } from "@mui/material";
+import React, { useContext } from "react";
+import { ColorModeContext, tokens } from "../../theme";
+
+import {
+  LightModeOutlined,
+  DarkModeOutlined,
+  NotificationsOutlined,
+  SettingsOutlined,
+  PersonOutlined,
+  Search,
+} from "@mui/icons-material";
+
+const Topbar = () => {
+  const theme = useTheme();
+  const colors = tokens(theme.palette.mode);
+  const colorMode = useContext(ColorModeContext);
+
+  return (
+    <Box p={2} display="flex" justifyContent="space-between">
+      {/* SEARCH BAR */}
+      <Box
+        display="flex"
+        backgroundColor={colors.primary[400]}
+        borderRadius="3px"
+      >
+        <InputBase sx={{ ml: 2, flex: 1 }} placeholder="Search" />
+        <IconButton type="button" sx={{ p: 1 }}>
+          <Search />
+        </IconButton>
+      </Box>
+
+      {/* ICONS */}
+      <Box display="flex">
+        <IconButton onClick={colorMode.toggleColorMode}>
+          {theme.palette.mode === "dark" ? (
+            <DarkModeOutlined />
+          ) : (
+            <LightModeOutlined />
+          )}
+        </IconButton>
+        <IconButton>
+          <NotificationsOutlined />
+        </IconButton>
+        <IconButton>
+          <SettingsOutlined />
+        </IconButton>
+        <IconButton>
+          <PersonOutlined />
+        </IconButton>
+      </Box>
+    </Box>
+  );
+};
+
+export default Topbar;
